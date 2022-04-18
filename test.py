c98fc281cbd062da1f4807b12ae73e71eee06a6c
Braden Helmer <bradenhelmeraus@gmail.com>
2022-04-18 12:00:01 -0400

    Successful connection in test.py

---
+++ test.py
@@ -0,0 +1,9 @@
+import mysql.connector
+
+db = mysql.connector.connect(
+    host = "crimson.crjdaartjksp.us-east-1.rds.amazonaws.com",
+    username ="braden",
+    password="braden",
+    database="project"
+)
+
