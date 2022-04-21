0b4f67c9524d0ac57f130b6a19007a59f7c57ec1
Braden Helmer <bradenhelmeraus@gmail.com>
2022-04-21 17:07:21 -0400

    debug code

--- test.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-db = mysql.connector.connect(
-    host = "crimson.crjdaartjksp.us-east-1.rds.amazonaws.com",
-    username ="braden",
-    password="braden",
-    database="project"
-)
-
