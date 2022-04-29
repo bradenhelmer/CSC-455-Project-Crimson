36eba7b10ded7923a1bfaa991830e387cf3c9310
Braden Helmer <bradenhelmeraus@gmail.com>
2022-04-29 10:39:18 -0400

    table design??

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask
+from flask import Flask, request
 from flask import render_template
+from flask import url_for
+from flask import flash
+from flask import redirect
 
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = 'ac595d158a0b804c36102ec03c7355d5e501bd66f7e0ad2b'
 
 @app.route("/")
 def home():
@@ -23,3 +26,13 @@ def erd_diagram():
 @app.route("/logical_schema")
 def logical_schema():
     return render_template("logicalSchema.html")
+
+@app.route("/tbl_dgn_rel_data", methods=('GET', 'POST'))
+def table_design():
+    if request.method == 'POST':
+        name = request.form['name']
+        
+
+
+
+    return render_template("tblDgnRelData.html")
