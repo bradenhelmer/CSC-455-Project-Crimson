f58731ab34e48de468c3aed6249315f32fe2aa8d
Braden Helmer <bradenhelmeraus@gmail.com>
2022-05-02 13:08:33 -0400

    Supporting queries pagE

--- app.py
+++ app.py
@@ -3,7 +3,17 @@ from flask import render_template
 from flask import url_for
 from flask import flash
 from flask import redirect
+import mysql.connector;
 
+
+db = mysql.connector.connect(
+    host="crimson.crjdaartjksp.us-east-1.rds.amazonaws.com",
+    username="braden",
+    password="braden",
+    database="project"
+)
+
+cursor = db.cursor()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ac595d158a0b804c36102ec03c7355d5e501bd66f7e0ad2b'
@@ -27,6 +37,13 @@ def erd_diagram():
 def logical_schema():
     return render_template("logicalSchema.html")
 
-@app.route("/tbl_dgn_rel_data", methods=('GET', 'POST'))
+@app.route("/tbl_dgn_rel_data")
 def table_design():
     return render_template("tblDgnRelData.html")
+
+@app.route("/supporting_queries")
+def supporting_queries():
+    
+    cursor.execute("SELECT * FROM pokemon;")
+    
+    return render_template('supportingQueries.html', cursor=cursor)
